b8577508b801bd186560d3775df0f967d04681e2
Caner Ekinci <canereknc37@gmail.com>
2024-05-05 23:38:48 +0300

    feat: static features added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from pathlib import Path
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+
 from .api.main import api_router
 from . import models
 from .core import db
 
+BASE_DIR = Path(__file__).resolve().parent
 models.Base.metadata.create_all(bind=db.engine)
 
 app = FastAPI()
+app.mount(
+    "/static",
+    StaticFiles(directory=str(BASE_DIR) + "/../../frontend/src/static"),
+    name="static",
+)
+
 app.include_router(api_router)
