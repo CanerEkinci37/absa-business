3fab0d5e13b1a68ddecb3fd919115e3ca6dc5d77
Caner Ekinci <canereknc37@gmail.com>
2024-05-05 23:42:57 +0300

    feat: business_report router added

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
-from .routes import business_category, business, aspect
+from .routes import index, business_category, business, aspect, business_report
 
 api_router = APIRouter()
+api_router.include_router(index.router)
 api_router.include_router(business_category.router)
 api_router.include_router(business.router)
 api_router.include_router(aspect.router)
+api_router.include_router(business_report.router)
