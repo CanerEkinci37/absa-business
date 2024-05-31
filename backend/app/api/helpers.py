c1c6cf18971bc9dcb547fda2d085ca531818f765
Caner Ekinci <canereknc37@gmail.com>
2024-05-31 20:45:31 +0300

    refactor: get_models function changed

--- backend/app/api/helpers.py
+++ backend/app/api/helpers.py
@@ -1,9 +1,18 @@
+import re
+import os
 from io import BytesIO
 from pathlib import Path
 from fastapi import UploadFile
 import numpy as np
 import pandas as pd
+import spacy
+import nltk
+from nltk.corpus import stopwords
+from zemberek import TurkishMorphology, TurkishSentenceNormalizer
 import joblib
+
+
+nlp = spacy.load("tr_core_news_md")
 
 
 def file_handle(file: UploadFile):
@@ -25,16 +34,49 @@ def extract_col(df, colname):
 path = Path(__file__).parent
 
 
-def get_ml_models():
-    with open(path / "../saved_models/restaurant/food_model.joblib", "rb") as f:
-        food_model = joblib.load(f)
-    with open(path / "../saved_models/restaurant/price_model.joblib", "rb") as f:
-        price_model = joblib.load(f)
-    with open(path / "../saved_models/restaurant/service_model.joblib", "rb") as f:
-        service_model = joblib.load(f)
-    with open(path / "../saved_models/restaurant/ambience_model.joblib", "rb") as f:
-        ambience_model = joblib.load(f)
-    return food_model, price_model, service_model, ambience_model
+def get_models(category):
+    model_dict = {}
+    current_path = path / f"../saved_models/{category}"
+    file_list = os.listdir(current_path)
+    for file in file_list:
+        with open(current_path / file, "rb") as f:
+            model = joblib.load(f)
+        name = file.split("_")[0]
+        model_dict[name] = model
+    print(model_dict)
+    return model_dict
+
+
+def add_counts_with_preds(preds):
+    unique, counts = np.unique(preds, return_counts=True)
+    sentiment_dict = dict(zip(unique, counts))
+    results = {
+        "NEGATIVE": sentiment_dict.get(-1, 0),
+        "NEUTRAL": sentiment_dict.get(0, 0),
+        "POSITIVE": sentiment_dict.get(1, 0),
+    }
+    return results
+
+
+def preprocess(col):
+    nltk.download("punkt")
+    stop_words = set(stopwords.words("turkish"))
+    morphology = TurkishMorphology.create_with_defaults()
+    sentence_normalizer = TurkishSentenceNormalizer(morphology)
+    for i in range(len(col)):
+        text = col[i]
+        text = sentence_normalizer.normalize(text).lower()
+        text = re.sub("[^a-zA-Zçğıöşü ]+", "", text)
+        text = re.sub("[ ]+", " ", text)
+        doc = nlp(text)
+        filtered_tokens = [
+            token.lemma_
+            for token in doc
+            if token.text not in stop_words and not token.is_oov
+        ]
+        col[i] = " ".join(filtered_tokens)
+
+    return col
 
 
 def get_model_predict_results(model, col):
@@ -43,15 +85,3 @@ def get_model_predict_results(model, col):
     # {-1: preds, 0:preds, 1:preds}
     result_dict = dict(zip(unique, counts))
     return result_dict
-
-
-def get_business_dict():
-    business_dict = {"FOOD": {}, "PRICE": {}, "SERVICE": {}, "AMBIENCE": {}}
-    return business_dict
-
-
-def add_result_to_dict(aspect, business_dict, results_dict):
-    business_dict[aspect]["NEGATIVE"] = results_dict[-1]
-    business_dict[aspect]["NEUTRAL"] = results_dict[0]
-    business_dict[aspect]["POSITIVE"] = results_dict[1]
-    return business_dict
